6d1d40c5d34b1af9c15b27605cbeffeee87e11eb
Deyvid Lopes <deyvidyury@yahoo.com.br>
2019-12-19 00:09:52 -0300

    Creating project and git repository

---
+++ src/db/DbIntegrityException.java
@@ -0,0 +1,14 @@
+package db;
+
+/**
+ * DbIntegrityException
+ */
+public class DbIntegrityException extends RuntimeException {
+
+  private static final long serialVersionUID = 1L;
+
+  public DbIntegrityException(String msg) {
+    super(msg);
+  }
+
+}
