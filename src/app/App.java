b886d94c7e66d0f201e5447752fc6aa7d01c7bc5
Deyvid Lopes <deyvidyury@yahoo.com.br>
2019-12-23 16:44:56 -0300

    findById implementation

--- src/app/App.java
+++ src/app/App.java
@@ -1,7 +1,13 @@
 package app;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
+import model.entities.Seller;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello Java");
+        SellerDao sellerDao = DaoFactory.createSellerDao();
+        Seller seller = sellerDao.findById(3);
+        System.out.println(seller);
     }
 }
